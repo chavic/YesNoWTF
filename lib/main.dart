9238fcede904f8d1341af21d2e37e8eff795ec33
chavic <43620557+chavic@users.noreply.github.com>
2019-12-23 02:18:15 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+//import 'injection_container.dart' as di;
+import 'presentation/route_generater.dart';
+
+void main() async {
+  //await di.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      );
+  }
+}
---
+++ lib/presentation/route_generater.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:template/main.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting the Arguments passed when [Navigator.pushNamed] is called
+    final args = settings.arguments;
+
+    // Navigating to the route passed when [Navigator.pushNamed] is called
+    switch (settings.name) {
+      // Defult Route
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+        break;
+      case "other page":
+        if (args) {
+          // Do Someting with the arguments
+
+        } else {
+         return _errorRoute();
+        }
+        break;
+      default:
+        return _errorRoute();
+    }
+
+    return _errorRoute();
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(
+      builder: (_) =>  Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Navigation error'),
+          ),
+        ),
+      ),
+    );
+  }
+}
